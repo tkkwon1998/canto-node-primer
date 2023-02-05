d8ce101be4a040dac6adc9b91415f560b669b894
T.K. Kwon <tkkwon1998@gmail.com>
2023-02-05 17:16:07 -0500

    update script to work in one pass

--- setup.sh
+++ setup.sh
@@ -3,25 +3,26 @@
 set -e
 
 # Updates system and installs dependencies
-sudo apt-get -y update && sudo apt-get -y upgrade
-sudo snap install go --classic && sudo apt-get install git && sudo apt-get install gcc && sudo apt-get -y install make
+sudo apt update
+sudo apt install make && sudo apt install gcc
+wget https://golang.org/dl/go1.18.3.linux-amd64.tar.gz
+sudo tar -xvf go1.18.3.linux-amd64.tar.gz -C /usr/local
+sudo chown -R plex:plex /usr/local/go
+echo "export GOPATH=$HOME/go \nexport PATH=$PATH:$GOPATH/bin:/usr/local/go/bin"  | tee -a ~/.profile
+
+cd ..
 git clone https://github.com/Canto-Network/Canto.git
 cd Canto
 git checkout v5.0.0
 
 # Installs and builds Canto daemon program
-cd $HOME/Canto/cmd/cantod
-go install -tags ledger ./...
-sudo mv $HOME/go/bin/cantod /usr/bin/
+make install
 
-cd $HOME/Canto
-sudo make install
+rm -rf ~/.cantod/config/genesis.json
 wget https://raw.githubusercontent.com/Canto-Network/Canto/genesis/Networks/Mainnet/genesis.json -P $HOME/.cantod/config/
 
 # Syncs node
 cd $HOME/canto-node-primer
 chmod 700 state_sync.sh
 ./state_sync.sh
-sudo service canto stop
-cantod unsafe-reset-all
-sudo service canto start
+cantod start
